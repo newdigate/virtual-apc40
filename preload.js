5e73500fb93d9620ebab6a6676087b53fd48d2d3
Newdigate <nicnewdigate@gmail.com>
2022-06-12 21:20:15 +0100

    sliders trigger midi cc events

--- preload.js
+++ preload.js
@@ -2,7 +2,7 @@ const {ipcRenderer} = require('electron');
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
-	var nodePads = document.querySelectorAll('.channelNotePad');
+	let nodePads = document.querySelectorAll('.channelNotePad');
 
 	for (let i = 0; i < nodePads.length; i++) {
 		nodePads[i].addEventListener('click', (e) => {
@@ -16,19 +16,29 @@ window.addEventListener('DOMContentLoaded', () => {
 		})
 	}
 
-	var ccKnobs = document.querySelectorAll('.cc-knob');
+	let ccKnobs = document.querySelectorAll('.cc-knob');
 	for (let i = 0; i < ccKnobs.length; i++) {
 		ccKnobs[i].addEventListener('input', (e) => {
-			if (e.target.dataset.cc !== undefined && e.target.dataset.channel !== undefined) {
-				var ccNum = parseInt(e.target.dataset.cc);
-				var channelNum = parseInt(e.target.dataset.channel);
-				var value = parseInt(e.detail.value);
-				ipcRenderer.invoke('some-name-cc', [ccNum, channelNum, value]).then((result) => {
-					console.log('cced... ' + result);
-				  })
-			}
+			triggerCCevent(e);
+		})
+	}
+
+	let sliders = document.querySelectorAll('.slider');
+	for (let i = 0; i < sliders.length; i++) {
+		sliders[i].addEventListener('input', (e) => {
+			triggerCCevent(e);
 		})
 	}
 })
 
+function triggerCCevent(e) {
+	if (e.target.dataset.cc !== undefined && e.target.dataset.channel !== undefined) {
+		var ccNum = parseInt(e.target.dataset.cc);
+		var channelNum = parseInt(e.target.dataset.channel);
+		var value = parseInt(e.detail.value);
+		ipcRenderer.invoke('some-name-cc', [ccNum, channelNum, value]).then((result) => {
+			console.log('cced... ' + result);
+		});
+	}
+}
 
